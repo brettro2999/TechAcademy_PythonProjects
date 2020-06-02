96f0445b341e50878ce199b788ee351f4c1b7d21
Brett Romero <brero2999@gmail.com>
2020-06-01 19:29:01 -0600

    sqlite 3 assignment
    push python sqlite

---
+++ database.py
@@ -0,0 +1,38 @@
+
+import sqlite3
+
+conn = sqlite3.connect('list.db')
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_txtFiles( \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+        file_name TEXT \
+        )")
+    conn.commit()
+conn.close()
+
+
+
+conn = sqlite3.connect('list.db')
+
+with conn:
+    cur = conn.cursor()
+    fileList = ('information.docx','Hello.txt','myImage.png', \
+                'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg')
+    for tFile in fileList:
+        if tFile.endswith(".txt"):
+            cur.execute("INSERT INTO tbl_txtFiles(file_name) VALUES (?)", \
+                        (tFile))
+    conn.commit()
+conn.close()
+
+conn = sqlite3.connect('list.db')
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("SELECT file_name FROM tbl_txtFiles")
+    varTxt = cur.fetchall()
+    for item in varTxt:
+        files = "() \n()".format(item[0],item[1])
+    print(files)

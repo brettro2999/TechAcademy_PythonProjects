b09334dbc0d6cce34962fbdf14d215555fc45be2
Brett Romero <brero2999@gmail.com>
2020-06-03 21:15:47 -0600

    parent child class
    class push

---
+++ parChild.py
@@ -0,0 +1,14 @@
+class Team:
+    name = "Cowboys"
+    city = "Dallas"
+    roster_size = 53
+
+class coaches(Team):
+    head = "John McCarthy"
+    assist = "Jerry Jones"
+
+class players(Team):
+    qb = "Dak Prescott"
+    rb = "Zeke Elliot"
+    lb = "Leighton Vander Esch"
+    
